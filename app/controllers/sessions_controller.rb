c222080fe3e28e5de6835d054c626ca84877bdec
AlexSYanai <yanai.alex@gmail.com>
2014-12-13 16:39:12 -0500

    resolves sessions controller conflict that didn't previously save

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,25 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-<<<<<<< HEAD
     user = User.find_by(username: params[:session][:username])
 
     if user && user.authenticate(params[:session][:password])
       login(user)
       redirect_to posts_path
     else
-    flash.now[:error] = "Invalid login"
-    render :new
-=======
-    @current_user = User.find_by(name: params[:session][:user_name])
-
-    if @current_user && @current_user.authenticate(params[:session][:user_password])
-      session[:user_id] = user.id
-      redirect_to posts_path
-    else
-      flash[:error] = "Invalid Login."
-      redirect_to login_path
->>>>>>> ac9e3484b19faabd3b8276151a7b9642d7786e6f
+      flash.now[:error] = "Invalid login"
+      render :new
     end
   end
 
