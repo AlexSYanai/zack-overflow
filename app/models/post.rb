d8ff3936b6991fbf78a1ee706135881147385855
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-13 16:41:53 -0500

    update models to correctly reference other models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ActiveRecord::Base
   validates  :title, presence: true, uniqueness: true
   validates  :content, presence: true, uniqueness: true
-  belongs_to :creator, class_name: "User" #source potentially
-  belongs_to :category
-  has_many   :comments, :dependent => :destroy
-  has_many   :votes, as: :votable
+
+  belongs_to :author, class_name: "User", foreign_key: "user_id"
+  # belongs_to :category
+
+  has_many :comments, :dependent => :destroy
+  has_many :votes, as: :votable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,11 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :username, presence: true, uniqueness: true
 
-  has_many :posts, foreign_key: :creator
-  has_many :comments, foreign_key: :creator
+  has_many :posts
+  has_many :comments
   has_many :votes
 
   def may_edit(item)
     self.is_admin || item.user.id == self.id
   end
 end
-
