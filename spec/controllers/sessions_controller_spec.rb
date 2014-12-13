9c9c174048bfb141e2b621a079e048481321115a
AlexSYanai <yanai.alex@gmail.com>
2014-12-13 18:51:09 -0500

    fixes minor issues with spec files

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -5,6 +5,7 @@ describe SessionsController do
 
   it "should send a invalid login request back to the login page" do
     expect(subject).to render_template(:new)
+  end
 
   describe "GET /logout" do
     it "should logout the current user" do
