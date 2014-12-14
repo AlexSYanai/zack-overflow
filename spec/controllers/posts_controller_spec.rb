f7c505b53c93f8ed6ff84f9b26f02c93698cb81d
AlexSYanai <yanai.alex@gmail.com>
2014-12-13 20:05:51 -0500

    Adds test of posts index page

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,6 +1,14 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe PostsController do
-#   describe "POST with parameters" do
-#   end
-# end
+describe PostsController do
+  let(:test_user) {FactoryGirl.create :user}
+  let(:test_post) {FactoryGirl.create :post}
+
+  describe "#index" do
+    it "adds a post to a list of all posts" do
+      get :index
+      expect(response).to be_success
+    end
+  end
+
+end
