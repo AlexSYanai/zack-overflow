7826c6f98dc6287ad93835a68b52043576c5180f
AlexSYanai <yanai.alex@gmail.com>
2014-12-13 21:58:33 -0500

    Adds redirect if post invalid and related test

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -15,6 +15,11 @@ describe PostsController do
   end
 
   describe "#new" do
+    it "should display a form that allows users to create new posts" do
+      post :new
+      expect(response).to render_template("new")
+    end
+
     it "should allow users to make new posts" do
       get :new
       expect(assigns(:post)).to be_a_new(Post)
@@ -29,10 +34,14 @@ describe PostsController do
       }.to change { Post.count }.by(1)
     end
 
-
     it "should redirect to a page showing the created post" do
       post :create, :post => { title: 'my title', content: 'my content' }
       expect(response).to redirect_to post_path(Post.all.last.id)
+    end
+
+    it "should redirect back to the login form upon false credentials" do
+      post :create, :post => { content: 'my content' }
+      expect(response).to redirect_to new_post_path
     end
   end
 
