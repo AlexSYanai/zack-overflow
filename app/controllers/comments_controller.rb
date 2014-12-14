7192b784f04b03613e4b08028916681f99557205
Chris Coles <cryan.coles@gmail.com>
2014-12-14 13:41:23 -0500

    deleted comments#index, moved displaying all coments to posts#show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
-      redirect_to post_comments_path
+      redirect_to post_path(@post)
   end
 
   def edit
