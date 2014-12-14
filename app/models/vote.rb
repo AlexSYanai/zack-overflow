521ca8d31c106a3903ea601f528237bf0616fbe9
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-14 18:21:25 -0500

    add validation to Vote model to prevent a user from voting on the same item twice

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,6 @@ class Vote < ActiveRecord::Base
   belongs_to :voter, class_name: "User", foreign_key: "user_id"
   belongs_to :votable, polymorphic: true
 
+  validates_uniqueness_of :user_id, :scope => :votable_id
+
 end
