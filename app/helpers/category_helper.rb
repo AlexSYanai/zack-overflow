8918df8dd856a7af8d9b94e11a3c0c6ce7d00f9b
Chris Coles <cryan.coles@gmail.com>
2014-12-14 18:33:00 -0500

    moved is_admin method to a helper file

--- app/helpers/category_helper.rb
+++ app/helpers/category_helper.rb
@@ -1,5 +1,6 @@
 module CategoryHelper
   def is_admin?
-    User.role.downcase == 'admin' || 'administrator'
+    user = User.find(params[:user_id])
+    user.role.downcase == 'admin' || user.role.downcase == 'administrator'
   end
 end
