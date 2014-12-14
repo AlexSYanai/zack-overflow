8baece22ecf14a0bbe3a3da2c83accc1a5941091
AlexSYanai <yanai.alex@gmail.com>
2014-12-14 01:21:16 -0500

    adds dropdown box to select category when creating new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:id, :content, :user_id, :title)
+      params.require(:post).permit(:id, :content, :user_id, :title, :category_id)
     end
 
 
