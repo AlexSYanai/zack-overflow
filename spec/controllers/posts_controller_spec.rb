3ff551f124cbd2dc846ca0719709c2ef59031fc7
AlexSYanai <yanai.alex@gmail.com>
2014-12-13 21:31:59 -0500

    Adds test for posts new route

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 describe PostsController do
   let(:test_user) {FactoryGirl.create :user}
   let(:test_post) {FactoryGirl.create :post}
+  before :each do
+    session[:user_id] = test_user.id
+  end
 
   describe "#index" do
     it "adds a post to a list of all posts" do
@@ -11,9 +14,15 @@ describe PostsController do
     end
   end
 
+  describe "#new" do
+    it "should allow users to make new posts" do
+      get :new
+      expect(assigns(:post)).to be_a_new(Post)
+    end
+  end
+
   describe "#create" do
-    it "creates the post if valid params" do
-      session[:user_id] = test_user.id
+    it "should create a new post if given valid params" do
       expect {
         post :create,
         :post => { title: 'my title', content: 'my content' }
