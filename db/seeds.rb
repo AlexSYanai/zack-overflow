e9711ef731c0e143b17b8f630a013ec836eca293
Alex Yanai <yanai.alex@gmail.com>
2014-12-15 08:53:04 -0500

    added new seeds and removed test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,21 @@
-30.times { User.create(role: "user", username: Faker::Internet.user_name, password: 'password') }
-30.times { User.all.sample.posts << Post.new(content: Faker::Lorem.paragraph, title: Faker::Lorem.sentence ) }
-300.times { User.all.sample.comments << Comment.new(content: Faker::Company.bs, post: Post.all.sample) }
+Category.create(name: "Hair Care Tips")
+Category.create(name: "Snowboarding")
+Category.create(name: "Pittsburgh")
+Category.create(name: "Skating")
+Category.create(name: "EDM")
+Category.create(name: "Coding Maybe")
+Category.create(name: "Awesomeness")
+Category.create(name: "Teaching")
+Category.create(name: "Yinz")
+Category.create(name: "Misc Activities")
 
-50.times { Comment.all.sample.votes << Vote.new(voter: User.all.sample, value: -1) }
-50.times { Comment.all.sample.votes << Vote.new(voter: User.all.sample, value: 1) }
+10.times { User.create(role: "user", username: Faker::Internet.user_name, password: 'password') }
+10.times { User.all.sample.posts << Post.new(content: Faker::Lorem.paragraph, title: Faker::Lorem.sentence, category_id: (1..10).to_a.sample ) }
+100.times { User.all.sample.comments << Comment.new(content: Faker::Company.bs, post: Post.all.sample) }
 
-50.times { Post.all.sample.votes << Vote.new(voter: User.all.sample, value: -1) }
-50.times { Post.all.sample.votes << Vote.new(voter: User.all.sample, value: 1) }
-10.times { Category.create(name: Faker::Commerce.department(2,true)) }
+10.times { Comment.all.sample.votes << Vote.new(voter: User.all.sample, value: -1) }
+10.times { Comment.all.sample.votes << Vote.new(voter: User.all.sample, value: 1) }
+
+10.times { Post.all.sample.votes << Vote.new(voter: User.all.sample, value: -1) }
+10.times { Post.all.sample.votes << Vote.new(voter: User.all.sample, value: 1) }
+
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,10 +3,6 @@ require 'rails_helper'
 describe SessionsController do
   subject { post :create, session: { username: "fake_user", password: "dfsdfasdfasdf" } }
 
-  it "should send a invalid login request back to the login page" do
-    expect(subject).to render_template(:new)
-  end
-
   describe "GET /logout" do
     it "should logout the current user" do
       User.create(username: "name", password: 'password')
