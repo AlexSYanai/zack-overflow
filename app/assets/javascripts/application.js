e3ba52acbf0134faf0c023554d29393d50331f53
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-15 00:49:32 -0500

    remove JavaScript code that was causing errors in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,3 @@
-$(document).ready(function(){
-  $('h1').css({ 'width':'100%', 'text-align':'center' });
-  var h1 = $('h1').height();
-  var h = h1/2;
-  var w1 = $(window).height();
-  var w = w1/2;
-  var m = w - h
-  $('h1').css("margin-top",m + "px")
-});
-
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
